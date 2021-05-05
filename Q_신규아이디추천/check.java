0a298e52c542979adfbfb5da6b71528da5593890
YimKeul <leesjun29@naver.com>
2021-05-05 16:46:12 +0900

    KAKAO BLIND RECRUITMENT_Q_신규아이디추천_Deque

---
+++ Q_신규아이디추천/check.java
@@ -0,0 +1,14 @@
+package Q_신규아이디추천;
+
+public class check {
+	public static void main(String args[]) {
+		char text = 'a';
+		System.out.println(text);
+		if(text-'a' == 0 ) {
+			System.out.println("id");
+		}
+		char tt = 'z';
+		System.out.println(tt-'a');
+		System.out.println(text-'a');
+	}
+}
